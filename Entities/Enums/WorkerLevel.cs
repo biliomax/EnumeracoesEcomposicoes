8893bbe398aa6fd29548703817f90893a7088d69
Raimax Moura <biliorx@gmail.com>
2023-12-15 21:26:13 -0300

    Feature: Implementation of classes and methods for worker.

---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace EnumeracoesEcomposicoes.Entities.Enums
+{
+    internal enum WorkerLevel : int
+    {
+        Junior = 0,
+        MidLevel = 1,
+        Senior = 2
+    }
+}
---
+++ Entities/HourContract.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeracoesEcomposicoes.Entities
+{
+    internal class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuesPerHour { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuesPerHour, int hours)
+        {
+            Date = date;
+            ValuesPerHour = valuesPerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuesPerHour;
+        }
+    }
+}
